64d1f41a4a87e2590db63c13a35566c0a0e4b52e
hueindahaus <huanga@student.chalmers.se>
2024-07-01 01:00:29 +0200

    initial commit

--- keyboards/splitkb/aurora/lily58/rev1/keymaps/vial/keymap.c
+++ keyboards/splitkb/aurora/lily58/rev1/keymaps/vial/keymap.c
@@ -1,13 +1,58 @@
 #include QMK_KEYBOARD_H
 
+enum custom_keycodes {
+    CKC_LT = QK_KB_0,
+    CKC_RT,
+};
+
+bool is_shift_active(void) {
+	uint8_t mods = keyboard_report->mods;
+	return mods & MOD_BIT(KC_LSFT) || mods & MOD_BIT(KC_RSFT);
+};
+
+bool is_alt_active(void) {
+	uint8_t mods = keyboard_report->mods;
+	return mods & MOD_BIT(KC_LALT) || mods & MOD_BIT(KC_RALT);
+};
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+	switch (keycode) {
+		case CKC_LT:
+            if (record->event.pressed) { 
+				if (is_shift_active()) {
+					tap_code16( LALT(KC_8)); // [
+				} else if(is_alt_active()) {
+					tap_code16( LALT(KC_7)); // {
+				} else {
+					tap_code16( LSFT(KC_8)); // (
+				}
+			 }
+            return false;
+        case CKC_RT:
+            if (record->event.pressed) { 
+				if (is_shift_active()) {
+					tap_code16( LALT(KC_9)); // ]
+				} else if(is_alt_active()) {
+					tap_code16( LALT(KC_0)); // }
+				} else {
+					tap_code16( LSFT(KC_9)); // )
+				}
+			 }
+            return false;
+	}
+	
+
+	return true;
+};
+
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       [0] = LAYOUT(
-	KC_ESC,  KC_1, KC_2, KC_3,    KC_4,    KC_5,                    KC_6,  KC_7,    KC_8,    KC_9,  KC_0,     KC_GRV,
-	KC_TAB,  KC_Q, KC_W, KC_E,    KC_R,    KC_T,                    KC_Y,  KC_U,    KC_I,    KC_O,  KC_P,     KC_MINS,
-	KC_LCTL, KC_A, KC_S, KC_D,    KC_F,    KC_G,                    KC_H,  KC_J,    KC_K,    KC_L,  KC_SCLN,  KC_QUOT,
-	KC_LSFT, KC_Z, KC_X, KC_C,    KC_V,    KC_B,  KC_LBRC, KC_RBRC, KC_N,  KC_M,    KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
-	                     KC_LALT, KC_LGUI, MO(1), KC_SPC,  KC_ENT,  MO(2), KC_BSPC, KC_RGUI
+	KC_ESC,  KC_1, KC_2, KC_3,    KC_4,    KC_5,                 	  KC_6,  KC_7,    KC_8,    KC_9,  KC_0,     KC_GRV,
+	KC_TAB,  KC_Q, KC_W, KC_E,    KC_R,    KC_T,                  	  KC_Y,  KC_U,    KC_I,    KC_O,  KC_P,     KC_MINS,
+	KC_LSFT, KC_A, KC_S, KC_D,    KC_F,    KC_G,                   	  KC_H,  KC_J,    KC_K,    KC_L,  KC_SCLN,  KC_QUOT,
+	KC_LCTL, KC_Z, KC_X, KC_C,    KC_V,    KC_B,   CKC_LT, 	CKC_RT, KC_N,  KC_M,    KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
+	                     KC_LALT, KC_LGUI, MO(1), KC_SPC,  KC_ENT,  	MO(2), KC_BSPC, KC_RGUI
       ),
       [1] = LAYOUT(
 	_______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______,
@@ -42,3 +87,5 @@ const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
     [3] = { ENCODER_CCW_CW(_______, _______), ENCODER_CCW_CW(_______, _______) },
 };
 #endif
+
+//TODO OS SPECIFIC KEYMAPS: https://qmk.github.io/qmk_docs_devel/#/feature_os_detection
